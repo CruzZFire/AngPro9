0be8bbbdc82d753ff2a491120b38264dec337df7
CruzZFire <a.cruzana88@gmail.com>
2020-11-01 17:37:05 +0100

    dockerfile and express config

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const https = require("https");
 const fs = require("fs");
 const history = require("connect-history-api-fallback");
 const jsonServer = require("json-server");
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const auth = require("./authMiddleware");
 const router = jsonServer.router("serverdata.json");
 
 const enableHttps = false;
-const ssloptions = {};
+
+const ssloptions = {}
 
 if (enableHttps) {
-    ssloptions.cerf = fs.readFileSync("./ssl/sportsstore.crt");
+    ssloptions.cert = fs.readFileSync("./ssl/sportsstore.crt");
     ssloptions.key = fs.readFileSync("./ssl/sportsstore.pem");
 }
 
@@ -23,10 +24,12 @@ app.use("/api", router);
 app.use(history());
 app.use("/", express.static("./dist/SportsStore"));
 
-app.listen(80, () => console.log("http running on 80"));
+app.listen(80,
+    () => console.log("HTTP Server running on port 80"));
 
 if (enableHttps) {
-    https.createServer(ssloptions, app).listen(443, () => console.log("https running on 443"));
+    https.createServer(ssloptions, app).listen(443,
+        () => console.log("HTTPS Server running on port 443"));
 } else {
-    console.log("https disabled")
+    console.log("HTTPS disabled")
 }
